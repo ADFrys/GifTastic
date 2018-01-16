9d56939c836105281feac1c987cedf7781549e2c
AngelaF <ADFrys@users.noreply.github.com>
2018-01-16 14:44:05 -0600

    Render buttons.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,23 +3,24 @@ var topics = ["dog", "cat", "beagles", "fish", "hamsters"];
 // q: = query string (search term)
 // rating = "";
 
-function renderButtons(button) {
+function renderButtons() {
 	$(".animal-buttons").empty();
 
 	for (var i = 0; i < topics.length; i++) {
 	  var button = $("<button>");
 	  button.addClass("animal");
 	  button.attr("data", topics[i]);
-    buttons.text(topics[i]);
-    $(".animal-buttons").append(button);
-
+      button.text(topics[i]);
+      $(".animal-buttons").append(button);
     }
    }
+   renderButtons();
 
 $("#submit-animal").on("click", function(event) {
   event.preventDefault();
   var animal = $("#animal-add").val().trim();
   topics.push(animal);
+  renderButtons();
   console.log(topics);
   var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=yyoxkLPmDt7TpjPoz9cj2p46gyBI90CW&limit=10";
   $.ajax({
