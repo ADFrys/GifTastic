cf6e23601b02871949fd172896e584a8899a58ef
AngelaF <ADFrys@users.noreply.github.com>
2018-01-16 10:57:53 -0600

    Push animals entered into array.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,19 +3,32 @@ var topics = ["dog", "cat", "beagles", "fish", "hamsters"];
 // q: = query string (search term)
 // rating = "";
 
-$("#submit-animal").on("click", function() {
+function renderButtons(button) {
+	$(".animal-buttons").empty();
+
+	for (var i = 0; i < topics.length; i++) {
+	  var button = $("<button>");
+	  button.addClass("animal");
+	  button.attr("data", topics[i]);
+    buttons.text(topics[i]);
+    $(".animal-buttons").append(button);
+
+    }
+   }
+
+$("#submit-animal").on("click", function(event) {
   event.preventDefault();
-  var animal = $("#animal-add").val();
+  var animal = $("#animal-add").val().trim();
+  topics.push(animal);
+  console.log(topics);
   var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=yyoxkLPmDt7TpjPoz9cj2p46gyBI90CW&limit=10";
   $.ajax({
   	url: giphyURL,
   	method: "GET"
   })
   .then(function(response) {
-  	console.log(response.data.);
+  	console.log(response.data);
   	console.log(giphyURL);
-
-
   });
 
 
