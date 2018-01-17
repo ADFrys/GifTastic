2ffe751ee5e04b37a581e9df65366d45230fc601
AngelaF <ADFrys@users.noreply.github.com>
2018-01-17 14:53:58 -0600

    Animate images.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,4 @@
-var topics = ["dog", "cat", "beagles", "fish", "hamsters"];
+var topics = ["puppy", "cat", "fish", "bird", "beagle", "hamsters"];
 
 function renderButtons() {
 	$(".animal-buttons").empty();
@@ -33,14 +33,16 @@ $(".animal-buttons").on("click", ".animal", function() {
   	console.log(giphyURL);
     
     for (var j = 0; j<response.data.length; j++) {
-      var giphyStillURL = response.data[j].images.downsized_still.url;
+      // var giphyStillURL = response.data[j].images.downsized_still.url;
       var image = $("<img>");
-      image.attr("src", giphyStillURL);
       image.attr("alt", "giphy image");
+      image.attr("data-still", response.data[j].images.downsized_still.url);
+      image.attr("data-animate", response.data[j].images.downsized.url);
+      image.attr("src", response.data[j].images.downsized_still.url);
+      image.attr("data-state", "still")
       image.attr("class", "gifs")
-      image.attr("data-state", "still");
       $("#animal-gifs").prepend(image);
-      var giphyAnimateURL = response.data[j].images.downsized.url;
+      // var giphyAnimateURL = response.data[j].images.downsized.url;
       var rating = response.data[j].rating;
       console.log(rating);
       var p = $("<p>").html("Rating: " + rating);
@@ -50,8 +52,15 @@ $(".animal-buttons").on("click", ".animal", function() {
     $("#animal-gifs").on("click", ".gifs", function() {
     // Set attribute data state to animate and back to still when click on image
     // var gifState = $(this).attr("data-state", "animate");
-      var animateGif = $(this).attr("src", giphyAnimateURL);
-      console.log(animateGif);
+    var imageState = $(this).attr("data-state");
+    if (imageState === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else if (imageState === "animate") {
+    	$(this).attr("src", $(this).attr("data-still"));
+    	$(this).attr("data-state", "still");
+      }
     });
   });
 });
